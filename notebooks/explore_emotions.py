57fd70d202349a24054f70104399a324a791bb93
Tejas-hue <temporary123407@gmail.com>
2025-07-14 19:21:09 +0530

    Update explore_emotions.py

--- notebooks/explore_emotions.py
+++ notebooks/explore_emotions.py
@@ -1,9 +1,9 @@
 from datasets import load_dataset
 
-# Load GoEmotions
+
 dataset = load_dataset("go_emotions")
 
-# Get label names (28 total)
+
 label_names = dataset['train'].features['labels'].feature.names
 
 print("Label index to name mapping:")
